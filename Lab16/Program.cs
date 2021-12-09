0f606eaaf630d90a21ce77bd3ff75bd158ac9dea
Rassokhin Anton <antonrassohin@gmail.com>
2021-12-09 21:44:46 +0300

    Добавьте файлы проекта.

---
+++ Lab16/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace Lab16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] expArray = new string[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Введите последовательно: код товара, название товара, цену товара:");
+                ExpTovar exportTovar = new ExpTovar()
+                {
+                    codeTovar = Convert.ToInt32(Console.ReadLine()),
+                    nameTovar = Console.ReadLine(),
+                    cenaTovar = Convert.ToUInt32(Console.ReadLine()),
+                };
+                JsonSerializerOptions options = new JsonSerializerOptions()
+                {
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                    WriteIndented = true
+                };
+                string expJsonString = JsonSerializer.Serialize(exportTovar, options);
+                expArray[i] = expJsonString;
+                using (StreamWriter recordFile = new StreamWriter("TEST.json", true))
+                {
+                    recordFile.WriteLine(expArray[i]);
+                }
+            }
+            
+
+            // вторая часть
+            string impJsonString = File.ReadAllText("TEST.json");
+            var impTovar = JsonSerializer.Deserialize<List<ImpTovar>>(impJsonString);
+            var maxPrice = impTovar.Max(x => x.cenaTovar);
+            var maxName = impTovar.Last(x => x.cenaTovar == maxPrice).nameTovar;
+            Console.WriteLine("Самый дорогой товар - {0}", maxName);
+            Console.ReadKey();
+        }
+    }
+
+    class ExpTovar
+    {
+        public int codeTovar { get; set; }
+        public string nameTovar { get; set; }
+        public uint cenaTovar { get; set; }
+    }
+
+    class ImpTovar
+    {
+        public int codeTovar { get; set; }
+        public string nameTovar { get; set; }
+        public uint cenaTovar { get; set; }
+
+        /*public void SayCena()
+        {
+            Console.WriteLine(cenaTovar);
+        }*/
+    }
+}
---
+++ Lab16_задание1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode; 
+using System.Text.Json.Serialization;
+
+namespace Lab16_задание1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tovar tovar = new Tovar()
+            { 
+            }
+
+
+        }
+    }
+
+    class Tovar
+    {
+        public int codeTovar { get; set; }
+        public string nameTovar { get; set; }
+        public uint CenaTovar { get; set; }     
+    }
+}
